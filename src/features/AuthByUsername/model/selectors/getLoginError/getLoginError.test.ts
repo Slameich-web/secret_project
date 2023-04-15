f966b8df25f2cb4efce3bfcdf6cf18600ff9e8b5
Slameich-web <slameacer@mail.ru>
2023-04-16 00:53:42 +0300

    tests

---
+++ src/features/AuthByUsername/model/selectors/getLoginError/getLoginError.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { getLoginError } from './getLoginError';
+import { StateSchema } from 'app/providers/StoreProvider';
+
+describe('getLoginError', () => {
+  test('should return error', () => {
+    const state: DeepPartial<StateSchema> = {
+      loginForm: {
+        error: 'error'
+      }
+    };
+    expect(getLoginError(state as StateSchema)).toEqual('error');
+  });
+  test('should work with empty state', () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getLoginError(state as StateSchema)).toEqual('');
+  });
+});
